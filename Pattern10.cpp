56dd25db791fc761d839560b383b356735c3ee2f
Roronoa-17 <111036213+Roronoa-17@users.noreply.github.com>
2023-08-26 20:45:50 +0530

    pattern 10

---
+++ Pattern10.cpp
@@ -0,0 +1,28 @@
+/*
+AAA
+BBB
+CCC
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i <= n)
+    {
+        int j = 1;
+        while(j <= n)
+        {
+            char c = 'A' + i - 1;
+            cout<<c;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
